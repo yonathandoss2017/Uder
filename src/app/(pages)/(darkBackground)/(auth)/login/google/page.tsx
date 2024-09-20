09c1cf114c1a3cad728372883718db9abc6d9516
ana.dinardi <ana.dinardi@estudiantes.utec.edu.uy>
2024-09-20 18:01:22 -0300

    Login con google

--- src/app/(pages)/(darkBackground)/(auth)/login/google/page.tsx
+++ src/app/(pages)/(darkBackground)/(auth)/login/google/page.tsx
@@ -93,11 +93,12 @@ const GoogleAuthPage = () => {
                     } else {
                         document.cookie = `sessionToken=${response};path=/;max-age=30;samesite=lax;secure`;
                         if (document.cookie.includes('sessionToken')) {
+                            window.location.href = "/";
                             sessionData.user.sessionAPIToken = response; // Asigna el token de sesión al usuario
 
-                            update(sessionData); // Actualiza la sesión del usuario (NextAuth)
+                            //update(sessionData); // Actualiza la sesión del usuario (NextAuth)
 
-                            window.location.href = "/"; // Redirige al usuario a la página principal
+                            // Redirige al usuario a la página principal
                         }
                     }
                 }
