e3353aa89a0e9a42e33ae3361d97bbe5b31427b5
bruno.moura <bruno.moura@estudiantes.utec.edu.uy>
2024-09-22 16:31:44 -0300

    Reactivacion: -Modelo -Proveedor -Tipo de equipo

--- src/services/ProveedorService.ts
+++ src/services/ProveedorService.ts
@@ -120,6 +120,28 @@ export async function darBajaProveedor(id: number, token: string): Promise<void 
             'Content-Type': 'application/json' // Tipo de contenido JSON
         }
     };
+    // Realiza la petición a la API y retorna el resultado
+    return await fetchVoidWithErrorHandling(url, options);
+}
+
+/**
+ *
+ * @param id
+ * @param token
+ * @returns Promise<void> - Si la solicitud se realiza correctamente.
+ * @returns Promise<FetchAPIError>
+ */
+export async function reactivarProveedor(id: number, token: string): Promise<void | FetchAPIError>{
+    const url: string = `${SERVICE_PATH}/reactivar/${id}`; // URL de la petición a la API
+
+    // Opciones de la petición
+    const options: RequestInit = {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + token, // Authorization header con token JWT
+            'Content-Type': 'application/json' // Tipo de contenido JSON
+        }
+    };
 
     // Realiza la petición a la API y retorna el resultado
     return await fetchVoidWithErrorHandling(url, options);
