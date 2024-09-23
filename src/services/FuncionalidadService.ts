e8a41661ea82efcf49e83feb5819952814a7f2c9
ana.dinardi <ana.dinardi@estudiantes.utec.edu.uy>
2024-09-23 12:38:40 -0300

    Funcionalidad agregar

--- src/services/FuncionalidadService.ts
+++ src/services/FuncionalidadService.ts
@@ -9,6 +9,16 @@ import PermisoEnum from "@/types/enums/PermisoEnum";
 // URL base de la API REST de la API para las funcionalidades
 const SERVICE_PATH: string = process.env.NEXT_PUBLIC_BACKEND_API_URL + "/funcionalidades";
 
+/**
+ * Función para listar funcoionalidades.
+ * @param token
+ * @param size
+ * @param page
+ * @param fieldSort
+ * @param sortDirectionAsc
+ * @returns Promise<FuncionalidadDTO[]> - Lista de funcionalidades registradas
+ * @returns Promise<FetchAPIError> - Si ocurre un error en la solicitud o en el procesamiento de la respuesta.
+ */
 export async function listarFuncionalidades(
     token: string,
     size: number = 0, page: number = 1, fieldSort: "nombre", sortDirectionAsc: boolean = true,
@@ -36,6 +46,12 @@ export async function listarFuncionalidades(
     return await fetchBodyWithErrorHandling<FuncionalidadDTO[]>(url, options);
 }
 
+/**
+ * Función para contar las funcionalidades de una institucion
+ * @param idInstitucion
+ * @returns Promise<number> - Cantidad de funcionalidades registradas
+ * @returns Promise<FetchAPIError> - Si ocurre un error en la solicitud o en el procesamiento de la respuesta.
+ */
 export async function contarFuncionalidades(idInstitucion:number): Promise<number> {
     const url: string = `${SERVICE_PATH}/contar?idInstitucion=${idInstitucion}`; // URL de la petición a la API
     // Opciones de la petición
@@ -56,6 +72,13 @@ export async function contarFuncionalidades(idInstitucion:number): Promise<numbe
     });
 }
 
+/**
+ * Función para obtener los permisos de una funcionalidad.
+ * @param funcionalidad
+ * @param token
+ * @returns Promise<PermisoEnum[]> - Lista de permisos de la funcionalidad
+ * @returns Promise<FetchAPIError> - Si ocurre un error en la solicitud o en el procesamiento de la respuesta.
+ */
 export async function obtenerPermisosFuncionalidad(funcionalidad: FuncionalidadDTO): Promise<PermisoEnum[] | FetchAPIError> {
     const url: string = `${SERVICE_PATH}/permisos?id=${funcionalidad.id}`; // URL de la petición a la API
     // Opciones de la petición
@@ -70,6 +93,13 @@ export async function obtenerPermisosFuncionalidad(funcionalidad: FuncionalidadD
     return await fetchBodyWithErrorHandling<PermisoEnum[]>(url, options);
 }
 
+/**
+ * Función para modificar una funcionalidad.
+ * @param funcionalidad
+ * @param token
+ * @returns Promise<void> - Si la solicitud se realiza correctamente
+ * @returns Promise<FetchAPIError> - Si ocurre un error en la solicitud o en el procesamiento de la respuesta.
+ */
 export async function modificarFuncionalidad(funcionalidad: FuncionalidadDTO, token: string): Promise<void | FetchAPIError> {
     const url: string = `${SERVICE_PATH}/modificar`; // URL de la petición a la API
     const options: RequestInit = { // Opciones de la petición
@@ -83,4 +113,26 @@ export async function modificarFuncionalidad(funcionalidad: FuncionalidadDTO, to
 
     // Realiza la petición a la API y retorna el resultado
     return await fetchVoidWithErrorHandling(url, options);
+}
+
+/**
+ * Función para agregar una funcionalidad
+ * @param funcionalidad
+ * @param token
+ * @returns Promise<FuncionalidadDTO> - Si la solicitud se realiza correctamente
+ * @returns Promise<FetchAPIError> - Si ocurre un error en la solicitud o en el procesamiento de la respuesta.
+ */
+export async function agregarFuncionalidad(funcionalidad: FuncionalidadDTO, token: String): Promise<FuncionalidadDTO | FetchAPIError> {
+    const url: string = `${SERVICE_PATH}/agregar`; // URL de la petición a la API
+    const options: RequestInit = { // Opciones de la petición
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + token, // Cabecera de autorización con el token de sesión
+            'Content-Type': 'application/json' // Tipo de contenido JSON
+        },
+        body: JSON.stringify(funcionalidad) // Cuerpo de la petición
+    };
+
+    // Realiza la petición a la API y retorna el resultado
+    return await fetchBodyWithErrorHandling<FuncionalidadDTO>(url, options);
 }
