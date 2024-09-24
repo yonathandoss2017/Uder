08b85e008bfd66a32d360fe4e77a2908abc36761
roney freire <roney.freire@estudiantes.utec.edu.uy>
2024-09-24 00:38:09 -0300

    agregar Proveedor funcionando

--- src/validations/SchemaProveedor.ts
+++ src/validations/SchemaProveedor.ts
@@ -1,23 +1,13 @@
-
-// Importa zod para la validación de los datos
-import {z, ZodObject} from "zod";
-
+import { z, ZodObject } from "zod";
 
 const SchemaProveedor: ZodObject<any> = z.object({
-
-    // Validación del nombre del proveedor
     nombre: z.string()
         .min(1, "El nombre no puede estar vacío")
         .max(30, "El nombre no puede tener más de 30 caracteres")
-        .refine((value: string) => value.replaceAll(" ", "").length != 0, {
+        .refine((value: string) => value.replaceAll(" ", "").length !== 0, {
             message: "El nombre no puede estar vacío",
         }),
-
-    // Validación del país de origen
-    idPaisOrigen: z.preprocess(((val): string => val ? String(val) : ''), // Convierte el valor a String
-        z.string()
-            .min(1, "Debe seleccionar un país")),
-
+    idPaisOrigen: z.string().optional(),
 });
 
 export default SchemaProveedor;
