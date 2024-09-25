966c974d2acef701af62789ea1617b387f860506
ana.dinardi <ana.dinardi@estudiantes.utec.edu.uy>
2024-09-25 18:25:33 -0300

    Cambios PermisoEnum

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -76,6 +76,25 @@ const sidebarConfig = [
         ]
     },
     {
+        title: "Gestión Perfiles",
+
+        key: "perfiles",
+
+        submenus: [
+
+            {
+                title: "Registro Perfiles", path: "/perfiles/agregar", permission: PermisoEnum.ALTA_PERFIL
+
+            },
+            {
+                title: "Listado Perfiles", path: "/perfiles", permission: PermisoEnum.OBTENER_PERFILES
+
+            },
+        ]
+
+    },
+
+    {
         title: "Gestión Proveedores",
         key: "proveedores",
         submenus: [
@@ -92,10 +111,14 @@ const sidebarConfig = [
         key: "funcionalidades",
         submenus: [
             {
-                title: "Ingreso Funcionalidad", path: "/funcionalidades/agregar", permission: PermisoEnum.ALTA_FUNCIONALIDAD
+                title: "Ingreso Funcionalidad",
+                path: "/funcionalidades/agregar",
+                permission: PermisoEnum.ALTA_FUNCIONALIDAD
             },
             {
-                title: "Listado Funcionalidades", path: "/funcionalidades", permission: PermisoEnum.OBTENER_FUNCIONALIDADES
+                title: "Listado Funcionalidades",
+                path: "/funcionalidades",
+                permission: PermisoEnum.OBTENER_FUNCIONALIDADES
             },
         ]
     }
