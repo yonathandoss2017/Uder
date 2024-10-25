e0e9e7f8830be48cc05bf9fb577c7a0d3224a401
ana.dinardi <ana.dinardi@estudiantes.utec.edu.uy>
2024-10-25 01:04:54 -0300

    Sidebar condicion

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -171,7 +171,7 @@ const Sidebar = (): ReactElement | null => {
                 setPermissions(response);
             }
         })();
-    }, [clientData.id]); // Agrego sessionData como dependencia
+    }, [clientData.id, sessionData]); // Agrego sessionData como dependencia
 
     useEffect(() => {
         // Encuentra la sección cuyo submenú está siendo visualizado actualmente
@@ -191,7 +191,7 @@ const Sidebar = (): ReactElement | null => {
         }
         return openMenu === sectionKey ? styles.subMenuTitleOpen : "";
     };
-if (!clientData) return <LoadingPage/>
+if (!clientData || !clientData.id) return <LoadingPage/>
     return (
         <nav className={styles.sidebarContainer}>
             {/* Título de la barra lateral */}
