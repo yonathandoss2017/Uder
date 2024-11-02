1e2c45f6865dd4de3c630567421009e8521ca68e
ana.dinardi <ana.dinardi@estudiantes.utec.edu.uy>
2024-11-02 18:45:07 +0000

    Resuelto conflicto en server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const httpsOptions = {
 app.prepare().then(() => {
     https.createServer(httpsOptions, (req, res) => {
         handle(req, res);
-    }).listen(3000, 'localhost', () => {
-        console.log('Entra');
+    }).listen(3000, '172.16.20.8', () => {
+        console.log("Funciona la web con https");
     });
 });
