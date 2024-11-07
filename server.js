cc33f0295b5f279a2818dc0d2eb3b8f83049764a
ana.dinardi <ana.dinardi@estudiantes.utec.edu.uy>
2024-11-06 22:07:06 -0300

    Localhost para ejecucion local

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const httpsOptions = {
 app.prepare().then(() => {
     https.createServer(httpsOptions, (req, res) => {
         handle(req, res);
-    }).listen(3000, '172.16.20.8', () => {
+    }).listen(3000, 'localhost', () => {
         console.log("Funciona la web con https");
     });
 });
