9ceebfb20a4895a06d0ec1766ee590d1230a5cc2
ana.dinardi <ana.dinardi@estudiantes.utec.edu.uy>
2024-12-19 12:29:11 -0300

    Comentarios

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const https = require('https');
-const fs = require('fs');
-const next = require('next');
-const path = require('path');
+const https = require('https'); // Módulo para crear servidores HTTPS en Node.js.
+const fs = require('fs'); // Módulo para trabajar con el sistema de archivos.
+const next = require('next'); // Framework Next.js para aplicaciones React.
+const path = require('path');  // Módulo para manejar rutas de archivos y directorios.
 
+// Verificar si la aplicación está en modo desarrollo o producción.
 const dev = process.env.NODE_ENV !== 'production';
 console.log(`Aplicación iniciada en modo ${dev ? 'desarrollo' : 'producción'}`);
 
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const app = next({ dev }); // Inicializar la aplicación Next.js.
+const handle = app.getRequestHandler(); // Manejador de solicitudes de Next.js.
 
+// Obtiene los certificados para el servidor HTTPS.
 const httpsOptions = {
     key: fs.readFileSync(path.join(process.cwd(), 'key.pem')),
     cert: fs.readFileSync(path.join(process.cwd(), 'cert.pem'))
 };
 
+// Prepara la aplicación Next.js.
 app.prepare().then(() => {
     https.createServer(httpsOptions, (req, res) => {
         handle(req, res);
     }).listen(3000, 'localhost', () => {
-        console.log("Funciona la web con HTTPS");
+        console.log("Funciona la web con HTTPS"); // Mensaje de éxito.
     });
 });
